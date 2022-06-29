c84dcc16f50e437e1b72a471e2a7d3f94734d493
Vishnu S <68739148+S0Vishnu@users.noreply.github.com>
2022-06-29 10:34:13 +0530

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,138 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+//loader
+const textureloader = new THREE.TextureLoader()
+const normalTexture = textureloader.load('/textures/texture-5.png')
+
+// Debug
+// const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.BoxGeometry(.8, .8, .8);
+geometry.rotateZ = 20;
+// geometry.position(0, .5, 0)
+
+// Materials
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0
+material.roughness = .8
+material.normalMap = normalTexture
+material.color = new THREE.Color(0x3A5BA0)
+
+// Mesh
+const sphere = new THREE.Mesh(geometry, material)
+scene.add(sphere)
+
+// Lights
+
+const pointLight = new THREE.AmbientLight(0xffffff, .1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+const pointLight2 = new THREE.PointLight(0x7F8487, 1)
+pointLight2.position.set(1, -1, 1)
+pointLight2.intensity = 5
+scene.add(pointLight2)
+
+const pointLight3 = new THREE.PointLight(0x7F8487, 1)
+pointLight3.position.set(-1, 1, .3)
+pointLight3.intensity = 4
+scene.add(pointLight3)
+
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+var value = .5
+
+document.getElementById("left").addEventListener("click", left)
+
+function left() {
+    value = -.5
+}
+document.getElementById("right").addEventListener("click", right)
+
+function right() {
+    value = .5
+}
+
+const clock = new THREE.Clock()
+
+const tick = () => {
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+
+    sphere.rotation.y = value * elapsedTime
+
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
